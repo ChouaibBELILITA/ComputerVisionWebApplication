005c7f4d68329ec471936e6cdf365b8511e3b7a8
mahmoudEttouahri <37670274+mahmoudEttouahri@users.noreply.github.com>
2020-07-29 11:50:25 +0000

    adding streaming server to /api/__init__

---
+++ backend/src/streaming/multithreading.py
@@ -0,0 +1,27 @@
+import asyncio
+import threading
+import random
+from 
+
+def thr(i):
+    # we need to create a new loop for the thread, and set it as the 'default'
+    # loop that will be returned by calls to asyncio.get_event_loop() from this
+    # thread.
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    loop.run_until_complete(do_stuff(i))
+    loop.close()
+
+
+
+def main():
+    num_threads = 10
+    threads = [ threading.Thread(target = thr, args=(i,)) for i in range(num_threads) ]
+    [ t.start() for t in threads ]
+    [ t.join() for t in threads ]
+    print("bye")
+
+
+if __name__ == "__main__":
+    main()
--- backend/src/streaming/server.py
+++ backend/src/streaming/server.py
@@ -11,56 +11,12 @@ import base64
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-path = os.path.join(dir_path, 'video.mp4')
-output_path = os.path.join(dir_path, os.path.join('media', 'dash.mpd'))
-
-'''
-video = ffmpeg_streaming.input(path)
-
-dash = video.dash(Formats.h264())
-
-dash.auto_generate_representations([1080, 720, 480])
-dash.generate_hls_playlist()
-
-dash.output(output_path)
-
-
-
-_144p = Representation(Size(256, 144), Bitrate(95 * 1024, 64 * 1024))
-_240p = Representation(Size(426, 240), Bitrate(150 * 1024, 94 * 1024))
-
-
-dash = video.dash(Formats.h264())
-dash.representations(_144p, _240p)
-
-dash.output(output_path)
-
-
-# WS server example
-
-
-async def hello(websocket, path):
-    name = await websocket.recv()
-    print(f"< {name}")
-
-    greeting = f"Hello {name}!"
-
-    await websocket.send(greeting)
-    print(f"> {greeting}")
-
-start_server = websockets.serve(hello, "localhost", 8765)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()'''
-
-#!/usr/bin/env python
-
-# WS server that sends messages at random intervals
 
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture('video2.mp4')
+        path = os.path.join(dir_path, ('video2.mp4'))
+        self.video = cv2.VideoCapture(path)
 
     def __del__(self):
         self.video.release()
@@ -79,6 +35,7 @@ def gen(camera):
     res_bytes = json.dumps(dat).encode('utf-8')
 
     while True:
+
         image = next(camera.get_frame())
 
         yield(image)
@@ -86,10 +43,13 @@ def gen(camera):
 
 async def time(websocket, path):
     camera = VideoCamera()
+
     i = 0
     while True:
         i = i+1
+
         data = next(gen(camera))
+
         js = {'id': i, 'data': data}
         res_bytes = json.dumps(js).encode('utf-8')
         base6 = base64.b64encode(res_bytes)
@@ -98,7 +58,7 @@ async def time(websocket, path):
         await websocket.send(message)
 
 
-start_server = websockets.serve(time, "127.0.0.1", 5678)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+def run(i):
+    start_server = websockets.serve(time, "127.0.0.1", 5678+i)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
